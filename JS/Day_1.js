300dee0b9ef0d6dee7826d105fe100c4bde73bbd
Hazegard <m.catrice@outlook.com>
2018-12-01 12:59:33 +0100

    Day 1 - Part 2 - JS

--- JS/Day_1.js
+++ JS/Day_1.js
@@ -13,3 +13,23 @@ const partOne = () => {
 };
 
 partOne();
+
+const partTwo = () => {
+    inputs.getDay(1).then((res) => {
+        const frequencies = new Set([0]);
+        let freq = 0;
+        const values = res.split('\n').map(x => parseInt(x));
+        let i = 0;
+        while (true) {
+            freq += values[i % values.length];
+            if (frequencies.has(freq)) {
+                break
+            }
+            frequencies.add(freq);
+            i++;
+        }
+        console.log(freq);
+    })
+};
+
+partTwo()
