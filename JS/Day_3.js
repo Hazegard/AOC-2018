1b77590093d5eca041bf33bc0336cb6a848cdfad
Hazegard <m.catrice@outlook.com>
2018-12-03 14:03:05 +0100

    Day 3 - Part 2 - JS

--- JS/Day_3.js
+++ JS/Day_3.js
@@ -37,5 +37,49 @@ const partOne = () => {
 
 partOne();
 
+const partTwo = () => {
+  inputs.getDay(3).then((res) => {
+    const values = res.split('\n')
+      .map(e => {
+        const ee = e.split('@');
+        const id = ee[0].trim();
+        const v = ee[1].trim().split(':');
+        const o = v[0].split(',');
 
+        const origin = {x: parseInt(o[0].trim()), y: parseInt(o[1].trim())};
+        const d = v[1].split('x');
+        const dimens = {x: parseInt(d[0].trim()), y: parseInt(d[1].trim())};
+        return {
+          id,
+          origin,
+          dimens,
+        }
+      });
+    const array = Array(1000)
+      .fill(null)
+      .map(() => Array(1000).fill(0));
+    values.forEach((elt) => {
+      for (let x = elt.origin.x; x < elt.origin.x + elt.dimens.x; x++) {
+        for (let y = elt.origin.y; y < elt.origin.y + elt.dimens.y; y++) {
+          array[x][y] = array[x][y] + 1;
+        }
+      }
+    });
+    values.forEach((elt) => {
+      let isAlone = true;
+      l1:for (let x = elt.origin.x; x < elt.origin.x + elt.dimens.x; x++) {
+        for (let y = elt.origin.y; y < elt.origin.y + elt.dimens.y; y++) {
+          if (array[x][y] >= 2) {
+            isAlone = false;
+            break l1
+          }
+        }
+      }
+      if (isAlone) {
+        console.log(elt);
+      }
+    })
+  })
+};
 
+partTwo();
