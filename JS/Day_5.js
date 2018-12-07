7071bfbf887bd1e314269e10bcd330c6869f2a8c
Hazegard <m.catrice@outlook.com>
2018-12-07 09:31:14 +0100

    Day 5 - Part 2 - JS

--- JS/Day_5.js
+++ JS/Day_5.js
@@ -1,33 +1,63 @@
 const inputs = require('./Inputs');
 
-const partOne = () => {
-
+const getRedactedLength = (input) => {
   const reverseCase = (val) => {
     if (val.toUpperCase() === val) {
       return val.toLowerCase()
     }
     return val.toUpperCase();
   };
+
+  let prevLength = input.length;
+  let currLength = 0;
+  while (currLength !== prevLength) {
+    prevLength = currLength;
+    let prev = input[0];
+    for (let i = 1; i < input.length; i++) {
+      if (prev === reverseCase(input[i])) {
+        input[i - 1] = 0;
+        input[i] = 0;
+      }
+      prev = input[i]
+    }
+    input = input.filter(e => e !== 0);
+    currLength = input.length
+  }
+  return currLength;
+}
+
+const partOne = () => {
+
+
   inputs.getDay(5).then((res) => {
     let values = res.split('');
-    let prevLength = values.length;
-    let currLength = 0;
-    while (currLength !== prevLength) {
-      prevLength = currLength;
-      let prev = values[0];
-      for (let i = 1; i < values.length; i++) {
-        if (prev === reverseCase(values[i])) {
-          values[i - 1] = 0;
-          values[i] = 0;
-        }
-        prev = values[i]
-      }
-      values = values.filter(e => e !== 0);
-      currLength = values.length
-    }
-    console.log(currLength);
+
+
+    console.log(getRedactedLength(values));
   })
     .catch(err => console.log(err));
 };
 
 partOne();
+
+
+const partTwo = () => {
+  inputs.getDay(5).then((res) => {
+    const values = res.split('');
+    let minLength = values.length;
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    alphabet.forEach((letter) => {
+      const val = values.filter(e => {
+        return e !== letter && e !== letter.toUpperCase()
+      });
+      const currentLength = getRedactedLength(val);
+      if (currentLength < minLength) {
+        minLength = currentLength;
+      }
+    });
+    console.log('min');
+    console.log(minLength);
+  })
+};
+
+partTwo();
