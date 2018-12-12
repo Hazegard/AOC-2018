8646812f09d3395b2ac16dda3119efae047c2044
Hazegard <m.catrice@outlook.com>
2018-12-12 12:42:46 +0100

    Day 12 - Part 2 - JS

--- JS/Day_12.js
+++ JS/Day_12.js
@@ -15,7 +15,7 @@ const test = `initial state: #..#.#..##......###...###
 ###.. => #
 ###.# => #
 ####. => #`;
-const partOne = () => {
+const partOne = (iterations) => {
   inputs.getDay(12).then((res) => {
     res = res.split('\n\n');
     let pots = res[0].split(':')[1].trim().split('');
@@ -24,8 +24,15 @@ const partOne = () => {
       acc[r1[0].trim()] = r1[1].trim();
       return acc;
     }, {});
+    let diff = 0;
     let index0 = 0;
-    for (let k = 0; k < 20; k++) {
+    let sum = pots.reduce((acc, curr, i) => {
+      if (curr === '#') {
+      }
+      return curr === '#' ? acc + i - index0 : acc;
+    }, 0);
+
+    for (let k = 0; k < iterations; k++) {
       if (pots.slice(0, 5).includes('#')) {
         pots.unshift(...'.....'.split(''));
         index0 += 5;
@@ -38,16 +45,24 @@ const partOne = () => {
         newPot.push((notes[getState(pots, j)] || '.'));
       }
       newPot.push(...pots.slice(pots.length - 4, pots.length));
+      const newSum = newPot.reduce((acc, curr, i) => {
+        return curr === '#' ? acc + i - index0 : acc;
+      }, 0);
+      const newDiff = newSum - sum;
+      if (newDiff - diff === 0) {
+        sum = (iterations - k - 1) * newDiff + newSum;
+        break
+      }
+      sum = newSum;
       pots = newPot;
+      diff = newDiff;
     }
-    const response = pots.reduce((acc, curr, i) => {
-      return curr === '#' ? acc + i - index0 : acc;
-    }, 0);
-    console.log(response);
+    console.log(sum);
   });
   const getState = (pots, index) => {
     return pots.slice(index - 2, index + 3).join('')
   }
 };
 
-partOne();
+partOne(20);
+partOne(50000000000);
