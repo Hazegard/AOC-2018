41e94e3291d36dc873616018a798c820aa26d663
Hazegard <m.catrice@outlook.com>
2018-12-14 11:16:56 +0100

    Day 14 - Part 2 - JS

--- JS/Day_14.js
+++ JS/Day_14.js
@@ -2,12 +2,16 @@ const inputs = require('./Inputs');
 
 const partOne = () => {
   inputs.getDay(14).then((res) => {
+    const resString = res;
     const r = +res;
     res = 8 + res;
     const scoreboard = [3, 7];
     let elf1 = 0;
     let elf2 = 1;
-    while (res > 0) {
+    let resOne = false;
+    let k = 0;
+    let sequence = [];
+    while (true) {
       const receipt1 = scoreboard[elf1];
       const receipt2 = scoreboard[elf2];
       const sum = receipt1 + receipt2;
@@ -16,16 +20,43 @@ const partOne = () => {
       if (newReceipt1) {
         scoreboard.push(newReceipt1);
         res--;
+        if (+resString.toString().charAt(sequence.length) === newReceipt1) {
+          sequence.push(newReceipt1);
+          if (sequence.join('') === resString) {
+            break
+          }
+        } else {
+          sequence = [];
+          if (+resString.toString().charAt(sequence.length) === newReceipt1) {
+            sequence.push(newReceipt1);
+          }
+        }
+        k++;
       }
+
       res--;
       scoreboard.push(newReceipt2);
       elf1 = (elf1 + 1 + scoreboard[elf1]) % scoreboard.length;
       elf2 = (elf2 + 1 + scoreboard[elf2]) % scoreboard.length;
-      if (res <= 0) {
+      if (res <= 0 && !resOne) {
         const response = scoreboard.slice(r, r + 10).join('');
         console.log(response);
+        resOne = true;
+      }
+      k++;
+      if (+resString.toString().charAt(sequence.length) === newReceipt2) {
+        sequence.push(newReceipt2);
+        if (sequence.join('') === resString) {
+          break
+        }
+      } else {
+        sequence = [];
+        if (+resString.toString().charAt(sequence.length) === newReceipt2) {
+          sequence.push(newReceipt2);
+        }
       }
     }
+    console.log(k - 3);
   })
 };
 
