85098b6e86ab95c8710d695f8669719940815643
Hazegard <m.catrice@outlook.com>
2018-12-17 21:52:01 +0100

    Day 16 - Part 2 - JS

--- JS/Day_16.js
+++ JS/Day_16.js
@@ -69,7 +69,8 @@ const ops = [
   eqrr,
 ];
 const partOne = () => {
-  inputs.getDay(16).then((res) => {
+  return inputs.getDay(16).then((res) => {
+    const opPoss = {};
     let values = res.split('\n\n\n')[0]
       .split('\n\n')
       .map(e => {
@@ -82,20 +83,65 @@ const partOne = () => {
       });
     const response = values.reduce((acc, curr) => {
       let correct = 0;
-      ops.forEach((op) => {
+      ops.forEach((op, i) => {
+        const opCode = curr.instr[0];
         const ins = curr.instr.slice(1);
         const res = [...curr.input];
         op(res, ...ins);
         curr.result = res;
         if (curr.result.every((c, idx) => c === curr.output[idx])) {
           correct++;
+          if (!opPoss[opCode]) {
+            opPoss[opCode] = new Set([ops[i].name])
+          }
+          opPoss[opCode].add(ops[i].name);
         }
       });
       return correct >= 3 ? acc + 1 : acc;
     }, 0);
     console.log(response);
+    return Promise.resolve(opPoss)
   })
     .catch(err => console.log(err))
 };
 
-partOne();
+
+const partTwo = (dictOps) => {
+  inputs.getDay(16).then((res) => {
+    const operations = {};
+    while (Object.keys(dictOps).length !== 0) {
+      Object.keys(dictOps).forEach((op) => {
+        if (dictOps[op].size === 1) {
+          operations[op] = [...dictOps[op]][0];
+          delete dictOps[op];
+          Object.keys(dictOps).forEach((opp) => {
+            dictOps[opp].delete(operations[op])
+          });
+        }
+      });
+    }
+
+    const opFunc = Object.keys(operations).reduce((acc, curr) => {
+      acc[curr] = ops.filter((func) => operations[curr] === func.name)[0];
+      return acc
+    }, {});
+    let values = res.split('\n\n\n')[1]
+      .trim()
+      .split('\n')
+      .map(e => {
+        return e.split(' ').map(Number)
+      });
+    const reg = [0, 0, 0, 0];
+    for (const op of values) {
+      const opCode = op.shift();
+      opFunc[opCode](reg, ...op);
+    }
+    console.log(reg[0]);
+  })
+    .catch(err => console.log(err));
+};
+
+
+partOne().then((ops) => {
+  partTwo(ops)
+});
