ce3d9cb47a1294915cc603bd923d1c804b50d33f
Hazegard <m.catrice@outlook.com>
2018-12-18 18:25:48 +0100

    Day 18 - Part 2 - JS

--- JS/Day_18.js
+++ JS/Day_18.js
@@ -13,62 +13,79 @@ const getAdjacentCells = (x, y) => {
 };
 const partOne = () => {
   inputs.getDay(18).then((res) => {
-    let field = res.split('\n').map(e => e.split(''));
-    for (let _ = 0; _ < 10; _++) {
-      const newField = [];
-      for (let x = 0; x < field.length; x++) {
-        const newRow = [];
-        for (let y = 0; y < field[x].length; y++) {
-          const acre = field[x][y];
-          const adj = getAdjacentCells(x, y).map(c => (field[c[0]] || [])[c[1]] || null).reduce((acc, curr) => {
-            if (curr === OPEN) {
-              acc.open++;
-            } else if (curr === TREE) {
-              acc.tree++;
-            } else if (curr === LUMBERYARD) {
-              acc.lumbeyard++
-            }
-            return acc;
-          }, {open: 0, tree: 0, lumbeyard: 0});
-          if (acre === OPEN) {
-            if (adj.tree >= 3) {
-              newRow.push(TREE);
+      let field = res.split('\n').map(e => e.split(''));
+      const gridSave = new Map();
+      let turn = 1;
+      let turn2 = -1;
+      while (true) {
+        const newField = [];
+        for (let x = 0; x < field.length; x++) {
+          const newRow = [];
+          for (let y = 0; y < field[x].length; y++) {
+            const acre = field[x][y];
+            const adj = getAdjacentCells(x, y).map(c => (field[c[0]] || [])[c[1]] || null).reduce((acc, curr) => {
+              if (curr === OPEN) {
+                acc.open++;
+              } else if (curr === TREE) {
+                acc.tree++;
+              } else if (curr === LUMBERYARD) {
+                acc.lumbeyard++
+              }
+              return acc;
+            }, {open: 0, tree: 0, lumbeyard: 0});
+            if (acre === OPEN) {
+              if (adj.tree >= 3) {
+                newRow.push(TREE);
+              } else {
+                newRow.push(acre);
+              }
+            } else if (acre === TREE) {
+              if (adj.lumbeyard >= 3) {
+                newRow.push(LUMBERYARD);
+              } else {
+                newRow.push(acre);
+              }
+            } else if (acre === LUMBERYARD) {
+              if (adj.lumbeyard >= 1 && adj.tree >= 1) {
+                newRow.push(acre)
+              } else {
+                newRow.push(OPEN)
+              }
             } else {
-              newRow.push(acre);
+              console.log(acre)
             }
-          } else if (acre === TREE) {
-            if (adj.lumbeyard >= 3) {
-              newRow.push(LUMBERYARD);
-            } else {
-              newRow.push(acre);
+          }
+          newField.push(newRow);
+        }
+        field = newField;
+        if (turn === 10 || turn === turn2) {
+          let nbTree = 0;
+          let nbLumberyard = 0;
+          for (let x = 0; x < field.length; x++) {
+            for (let y = 0; y < field[x].length; y++) {
+              if (field[x][y] === TREE) {
+                nbTree++;
+              } else if (field[x][y] === LUMBERYARD) {
+                nbLumberyard++;
+              }
             }
-          } else if (acre === LUMBERYARD) {
-            if (adj.lumbeyard >= 1 && adj.tree >= 1) {
-              newRow.push(acre)
-            } else {
-              newRow.push(OPEN)
-            }
-          } else {
-            console.log(acre)
+          }
+          console.log(nbTree * nbLumberyard);
+          if (turn2 !== -1) {
+            return
           }
         }
-        newField.push(newRow);
-      }
-      field = newField;
-    }
-    let nbTree = 0;
-    let nbLumberyard = 0;
-    for (let x = 0; x < field.length; x++) {
-      for (let y = 0; y < field[x].length; y++) {
-        if (field[x][y] === TREE) {
-          nbTree++;
-        } else if (field[x][y] === LUMBERYARD) {
-          nbLumberyard++;
+        const fieldToSave = field.reduce((acc, curr) => acc + curr.join(''));
+        if (gridSave.has(fieldToSave)) {
+          const missingTurns = (1000000000 - gridSave.get(fieldToSave)) % (turn - gridSave.get(fieldToSave))
+          turn2 = turn + missingTurns;
+        } else {
+          gridSave.set(fieldToSave, turn);
         }
+        turn++;
       }
     }
-    console.log(nbTree * nbLumberyard)
-  })
+  )
     .catch(err => console.log(err));
 };
 
